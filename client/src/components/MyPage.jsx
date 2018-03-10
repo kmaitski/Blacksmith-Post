afced22c564bc3e8931abd530246fecede19855c
kdoddy <tuc36615@temple.edu>
2018-03-10 13:05:47 -0800

    displaying userinfo

--- client/src/components/MyPage.jsx
+++ client/src/components/MyPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
+import ReactDOM from 'react-dom';
 
 class MyPage extends React.Component {
   constructor(props) {
@@ -9,21 +10,19 @@ class MyPage extends React.Component {
       soldItems: [],
       boughtItems: []
     }
+
   }
 
-  componentWillMount() {
+  componentDidMount() {
     let thisUser = {username: this.props.user.local.username};
-    $.get('/userSells', thisUser, function(err, data) {
-      if (err) { console.log(err) }
-      console.log(data);
+    $.get('/userSells', thisUser, (data) => {
+      this.setState({soldItem: data.sells});
     })
-    $.get('/userBuys', thisUser, function(err, data) {
-      if (err) { console.log(err) }
-      console.log(data);
+    $.get('/userBuys', thisUser, (data) => {
+      this.setState({boughtItems: data.buys});
     })
-    $.get('/userCurrentItems', thisUser, function(err, data) {
-      if (err) { console.log(err) }
-      console.log(data);
+    $.get('/userCurrentItems', thisUser, (data) => {
+      this.setState({currentItems: data.currentItems});
     })
   }
 
@@ -31,6 +30,17 @@ class MyPage extends React.Component {
     return (
       <div style={{paddingTop: "5%"}}>
         <div className="container">{this.props.user.local.username}
+          <div className="container">
+          {this.state.currentItems &&
+            this.state.currentItems.map((item) => <div>Currently Listed: {item.name}</div>)
+          }
+          {this.state.soldItems &&
+            this.state.soldItems.map((item) => <div>Sold: {item.item}</div>)
+          }
+          {this.state.boughtItems &&
+            this.state.boughtItems.map((item) => <div>Bought: {item.item}</div>)
+          }
+          </div>
         </div>
       </div>
     )

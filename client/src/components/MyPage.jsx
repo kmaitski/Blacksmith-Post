d93bc4cfd64bb6a7a1f4c2dea4b58c2dbfb86efd
kdoddy <tuc36615@temple.edu>
2018-03-10 19:05:58 -0800

    heroku stuff

--- client/src/components/MyPage.jsx
+++ client/src/components/MyPage.jsx
@@ -40,41 +40,41 @@ class MyPage extends React.Component {
        <div>
         <div className="jumbotron">
           <h1 className="display-4 text-white">{this.props.user.local.username}</h1>
-          {this.state.thisUserRatings.length > 0 &&
+          {this.state.thisUserRatings &&
             <h2 className="display-4 text-white">{this.state.thisUserRatings[0].rating}</h2>
           }
           <div className="card-deck">
             <div className="card text-white bg-dark mb-3">
               <h4 className="card-title">Currently Listed Items</h4>
-              {!this.state.currentItems.length && <div>None so far . . .</div>}
+              {!this.state.currentItems && <div>None so far . . .</div>}
               {this.state.currentItems &&
                 this.state.currentItems.map((item) => <div>{item.name}</div>)
               }
             </div>
             <div className="card text-white text-center bg-dark mb-3">
               <h4 className="card-title">Sold items</h4>
-              {!this.state.soldItems.length && <div>None so far . . .</div>}
+              {!this.state.soldItems && <div>None so far . . .</div>}
               {this.state.soldItems &&
                 this.state.soldItems.map((item) => <div>Sold: {item.item}</div>)
               }
             </div>
             <div className="card text-white text-center bg-dark mb-3">
               <h4 className="card-title">User Reviews</h4>
-              {!this.state.thisUserFeedback.length && <div>None so far . . .</div>}
+              {!this.state.thisUserFeedback && <div>None so far . . .</div>}
               {this.state.thisUserFeedback &&
                 this.state.thisUserFeedback.map((review) => <div>{review.user} : {review.message}</div>)
               }
             </div>
             <div className="card text-white text-center bg-dark mb-3">
               <h4 className="card-title">Purchased items</h4>
-              {!this.state.boughtItems.length && <div>None so far . . .</div>}
+              {!this.state.boughtItems && <div>None so far . . .</div>}
               {this.state.boughtItems &&
                 this.state.boughtItems.map((item) => <div>Bought: {item.item}</div>)
               }
             </div>
             <div className="card text-white text-center bg-dark mb-3">
               <h4 className="card-title">My Feedback</h4>
-              {!this.state.ratings.length && <div>None so far . . .</div>}
+              {!this.state.ratings && <div>None so far . . .</div>}
             </div>
           </div>
         </div>
